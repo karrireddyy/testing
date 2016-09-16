722edaaba7984572410d49f902a636fa717e621d
karrireddyy1 <tarakram199+1@gmail.com>
2016-09-16 08:40:45 +0000

    deleted

--- hocr/test.sh
+++
@@ -1,5 +0,0 @@
-python /app/hocr/pdf2jpg.py
-
-python /app/hocr/jpg2hocr.py
-
-/app/hocr/hocr-pdf  /tmp/test/ . > "/tmp/test/some_0.pdf"
--- hocr/testing/uploading/test.sh
+++
@@ -1,5 +0,0 @@
-python pdf2jpg.py $1
-
-python jpg2hocr.py
-
-hocr-pdf  /tmp/test/ . > "/tmp/test/some_0.pdf"
